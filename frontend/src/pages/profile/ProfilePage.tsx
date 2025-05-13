8d964a73b0306888cac1e8169edd4f72307fbbbe
Aditya Chakma <adityachakma199@gmail.com>
2025-05-13 20:47:08 +0600

    Added Profile Page

---
+++ frontend/src/pages/profile/ProfilePage.tsx
@@ -0,0 +1,14 @@
+import UserDetails from "../../components/sections/UserDetails";
+
+const ProfilePage = () => {
+    return (
+        <>
+            <div>
+                <UserDetails />
+            </div>
+            {/* todo: Add posts */}
+        </>
+    );
+};
+
+export default ProfilePage;
